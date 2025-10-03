42fdd3f5d8fe98f2ce17ec71dcc84ee63e7954b2
fagotweb <aleksandr.19.88@mail.ru>
2025-10-03 19:01:04 +0300

    Домашка №2

---
+++ hw-2.js
@@ -0,0 +1,33 @@
+let a = 10;
+alert (a);
+a = 20;
+alert (a);
+const year = 1861;
+alert (year + " год");
+let n = "Яша Скрипкин";
+alert (n);
+let namber1 = 10;
+let namber2 = 2;
+alert (String(namber1 + namber2) + "," + String(namber1 - namber2) + "," + String(namber1 * namber2) + "," + String(namber1 / namber2));
+let result = 2**5
+alert (result);
+a = 9
+let b = 2
+alert (a % b);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+let age = prompt("Сколько вам лет?"); // Запрашиваем возраст
+alert(age); // Выводим возраст
+let user = {
+  name: "Яша Скрипкин",
+  age: 1861,
+  isAdmin: false
+};
+let name = prompt("Ваше имя?");
+alert(`Привет, ${name}!`);
